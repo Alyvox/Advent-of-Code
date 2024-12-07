7b052fa25e8beeedb0fb49cf554e5111ae74feed
koenb <koen.baltus@student.hu.nl>
2024-12-07 15:48:39 +0100

    AOC day 1

---
+++ 2024/Day 1/1-1.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('./Day 1/input.txt', 'utf-8');
+const lines = input.split(/\r?\n/);
+
+function main(){
+  const leftList: number[] = [];
+  const rightList: number[] = [];
+  let distance = 0
+  
+  lines.forEach((line) => {
+    const list = line.split(/(\s+)/).map(Number);
+    leftList.push(list[0]);
+    rightList.push(list[2]);
+  });
+  leftList.sort();
+  rightList.sort();
+
+  for(let i = 0; i < lines.length; i++){
+    distance += Math.abs(leftList[i] - rightList[i]);
+  }
+
+  console.log(distance);
+}
+
+main()
---
+++ 2024/Day 1/1-2.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('./Day 1/input.txt', 'utf-8');
+const lines = input.split(/\r?\n/);
+
+function main(){
+  const leftList: number[] = [];
+  const rightList: number[] = [];
+  let similiarity = 0
+
+  lines.forEach((line) => {
+    const list = line.split(/\s+/).map(Number);
+    leftList.push(list[0]);
+    rightList.push(list[2]);
+  });
+  leftList.sort();
+  rightList.sort();
+  
+  leftList.forEach((item)=> {
+    const count = rightList.filter((v) => (v === item)).length;
+    similiarity += item * count;
+  });
+
+  console.log(similiarity);
+}
+
+main()
